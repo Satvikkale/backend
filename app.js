51df46dc28d7c3a8667e6561289a3bba6d9a7b9c
Satvik Kale <satvikkale07@gmail.com>
2025-08-06 13:59:14 +0530

    update url

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ const Owner = require('./models/Owner');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://room-rental-website-6zrh.onrender.com",
+  credentials: true
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }))
 
