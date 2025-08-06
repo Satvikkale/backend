57701cc435e6809cdb2c0786c0922e501cc152ce
Satvik Kale <148377659+Satvikkale@users.noreply.github.com>
2025-08-06 15:00:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,49 @@ const Room = require('./models/Room');
 const bodyParser = require('body-parser');
 const User = require('./models/User');
 const Owner = require('./models/Owner');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
-// Middleware
+// ✅ CORS MUST come first
 app.use(cors({
-  origin: "https://room-rental-website-6zrh.onrender.com",
-  credentials: true
+    origin: "https://room-rental-website-6zrh.onrender.com",
+    credentials: true
 }));
 
+// ✅ Handle preflight (OPTIONS) requests
 app.options('*', cors({
-  origin: "https://room-rental-website-6zrh.onrender.com",
-  credentials: true
+    origin: "https://room-rental-website-6zrh.onrender.com",
+    credentials: true
 }));
 
+// ✅ Session middleware (now with Mongo store)
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        ttl: 14 * 24 * 60 * 60
+    }),
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None'
+    }
+}));
+
+// Logging for debug
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+// Middlewares
 app.use(express.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -94,24 +118,28 @@ app.delete('/api/rooms/:id', async (req, res) => {
     }
 });
 
+// ✅ User Login with session
 app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body;
 
-    if (req.body.email && req.body.password) {
-        let user = await User.findOne({ email: req.body.email })
-        if (user) {
+    if (email && password) {
+        const user = await User.findOne({ email });
+        if (user && user.password === password) {
+            req.session.user = {
+                email,
+                type: 'user'
+            };
             res.send(user);
         } else {
-            res.send({ result: 'Invalid email or password' });
+            res.status(401).send({ result: 'Invalid email or password' });
         }
     } else {
-        res.send({ result: 'Enter valid email or password' });
+        res.status(400).send({ result: 'Enter valid email and password' });
     }
 });
 
-
 app.post('/api/signup', async (req, res) => {
     const { name, email, password } = req.body;
-    console.log('Received signup request:', req.body); // Add logging
     try {
         const user = new User({ name, email, password });
         await user.save();
@@ -122,24 +150,27 @@ app.post('/api/signup', async (req, res) => {
     }
 });
 
+// ✅ Owner login with session
 app.post('/api/owner_login', async (req, res) => {
-
-    if (req.body.email && req.body.password) {
-        let user = await Owner.findOne({ email: req.body.email })
-        if (user) {
+    const { email, password } = req.body;
+    if (email && password) {
+        const user = await Owner.findOne({ email });
+        if (user && user.password === password) {
+            req.session.user = {
+                email,
+                type: 'owner'
+            };
             res.send(user);
         } else {
-            res.send({ result: 'Invalid email or password' });
+            res.status(401).send({ result: 'Invalid email or password' });
         }
     } else {
-        res.send({ result: 'Enter valid email or password' });
+        res.status(400).send({ result: 'Enter valid email or password' });
     }
 });
 
-
 app.post('/api/owner_signup', async (req, res) => {
     const { name, email, password, address, age, mobile } = req.body;
-    console.log('Received signup request:', req.body); // Add logging
     try {
         const user = new Owner({ name, email, password, address, age, mobile });
         await user.save();
@@ -150,9 +181,13 @@ app.post('/api/owner_signup', async (req, res) => {
     }
 });
 
+// ✅ Profile route using session
 app.get('/api/profile', async (req, res) => {
-    const { email, type } = req.query;
     try {
+        if (!req.session.user) {
+            return res.status(401).send('Unauthorized');
+        }
+        const { email, type } = req.session.user;
         let user;
         if (type === 'user') {
             user = await User.findOne({ email });
@@ -187,25 +222,234 @@ app.put('/api/profile', async (req, res) => {
 });
 
 app.get("/search/:key", async (req, res) => {
-    let result = await Room.find(
-        {
-            "$and": [
-                { status: true },
-                {
-                    "$or": [
-                        { price: { $regex: req.params.key, $options: 'i' } },
-                        { location: { $regex: req.params.key, $options: 'i' } }
-                    ]
-                }
-            ]
-        });
+    let result = await Room.find({
+        "$and": [
+            { status: true },
+            {
+                "$or": [
+                    { price: { $regex: req.params.key, $options: 'i' } },
+                    { location: { $regex: req.params.key, $options: 'i' } }
+                ]
+            }
+        ]
+    });
 
     res.send(result);
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
+
+
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const Room = require('./models/Room');
+// const bodyParser = require('body-parser');
+// const User = require('./models/User');
+// const Owner = require('./models/Owner');
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+
+
+// // Middleware
+// app.use(cors({
+//   origin: "https://room-rental-website-6zrh.onrender.com",
+//   credentials: true
+// }));
+
+// app.options('*', cors({
+//   origin: "https://room-rental-website-6zrh.onrender.com",
+//   credentials: true
+// }));
+
+// app.use(express.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// // MongoDB connection
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch(err => {
+//     console.error('Error connecting to MongoDB', err);
+// });
+
+// // Routes
+// app.post('/api/addroom', async (req, res) => {
+//     try {
+//         const room = new Room(req.body);
+//         await room.save();
+//         res.status(201).send(room);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// });
+
+// app.get('/api/rooms', async (req, res) => {
+//     try {
+//         const rooms = await Room.find();
+//         res.status(200).send(rooms);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.get('/api/rooms/:id', async (req, res) => {
+//     try {
+//         const room = await Room.findById(req.params.id);
+//         if (!room) {
+//             return res.status(404).send('Room not found');
+//         }
+//         res.status(200).send(room);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.put('/api/rooms/:id/availability', async (req, res) => {
+//     try {
+//         const room = await Room.findById(req.params.id);
+//         if (!room) {
+//             return res.status(404).send('Room not found');
+//         }
+//         room.status = req.body.status;
+//         await room.save();
+//         res.status(200).send(room);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// });
+
+// app.delete('/api/rooms/:id', async (req, res) => {
+//     try {
+//         const room = await Room.findByIdAndDelete(req.params.id);
+//         if (!room) {
+//             return res.status(404).send('Room not found');
+//         }
+//         res.status(200).send({ message: 'Room deleted successfully' });
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.post('/api/login', async (req, res) => {
+
+//     if (req.body.email && req.body.password) {
+//         let user = await User.findOne({ email: req.body.email })
+//         if (user) {
+//             res.send(user);
+//         } else {
+//             res.send({ result: 'Invalid email or password' });
+//         }
+//     } else {
+//         res.send({ result: 'Enter valid email or password' });
+//     }
+// });
+
+
+// app.post('/api/signup', async (req, res) => {
+//     const { name, email, password } = req.body;
+//     console.log('Received signup request:', req.body); // Add logging
+//     try {
+//         const user = new User({ name, email, password });
+//         await user.save();
+//         res.send('Sign-up successful');
+//     } catch (error) {
+//         console.error('Error signing up:', error);
+//         res.status(400).send('Error signing up');
+//     }
+// });
+
+// app.post('/api/owner_login', async (req, res) => {
+
+//     if (req.body.email && req.body.password) {
+//         let user = await Owner.findOne({ email: req.body.email })
+//         if (user) {
+//             res.send(user);
+//         } else {
+//             res.send({ result: 'Invalid email or password' });
+//         }
+//     } else {
+//         res.send({ result: 'Enter valid email or password' });
+//     }
+// });
+
+
+// app.post('/api/owner_signup', async (req, res) => {
+//     const { name, email, password, address, age, mobile } = req.body;
+//     console.log('Received signup request:', req.body); // Add logging
+//     try {
+//         const user = new Owner({ name, email, password, address, age, mobile });
+//         await user.save();
+//         res.send('Sign-up successful');
+//     } catch (error) {
+//         console.error('Error signing up:', error);
+//         res.status(400).send('Error signing up');
+//     }
+// });
+
+// app.get('/api/profile', async (req, res) => {
+//     const { email, type } = req.query;
+//     try {
+//         let user;
+//         if (type === 'user') {
+//             user = await User.findOne({ email });
+//         } else if (type === 'owner') {
+//             user = await Owner.findOne({ email });
+//         }
+//         if (!user) {
+//             return res.status(404).send('User not found');
+//         }
+//         res.status(200).send(user);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.put('/api/profile', async (req, res) => {
+//     const { email, type, profileImage } = req.body;
+//     try {
+//         let user;
+//         if (type === 'user') {
+//             user = await User.findOneAndUpdate({ email }, { profileImage }, { new: true });
+//         } else if (type === 'owner') {
+//             user = await Owner.findOneAndUpdate({ email }, { profileImage }, { new: true });
+//         }
+//         if (!user) {
+//             return res.status(404).send('User not found');
+//         }
+//         res.status(200).send(user);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+// app.get("/search/:key", async (req, res) => {
+//     let result = await Room.find(
+//         {
+//             "$and": [
+//                 { status: true },
+//                 {
+//                     "$or": [
+//                         { price: { $regex: req.params.key, $options: 'i' } },
+//                         { location: { $regex: req.params.key, $options: 'i' } }
+//                     ]
+//                 }
+//             ]
+//         });
+
+//     res.send(result);
+// });
+
+
+
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
 
